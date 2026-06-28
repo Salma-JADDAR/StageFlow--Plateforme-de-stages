c0b1138969266c714e2bf6ccfa48f7acb3227b72
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 19:29:49 +0100

    feat(company): extend entreprises schema with business sector and company size information

--- stage-platform/database/migrations/2026_06_12_170358_add_secteur_to_entreprises_table.php
+++ stage-platform/database/migrations/2026_06_12_170358_add_secteur_to_entreprises_table.php
@@ -4,25 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration{
+   
+    public function up(): void{
         Schema::table('entreprises', function (Blueprint $table) {
-            //
+           
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+   
+    public function down(): void {
         Schema::table('entreprises', function (Blueprint $table) {
-            //
+          
         });
     }
 };
