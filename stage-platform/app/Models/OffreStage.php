3fe8e7f5277c121a8830b73a0c35ca15851e91a5
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 19:54:46 +0100

    feat(model) : implémentation du modèle OffreStage avec gestion des relations, des statuts et des suppressions logiques

--- stage-platform/app/Models/OffreStage.php
+++ stage-platform/app/Models/OffreStage.php
@@ -6,9 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes; 
 
-class OffreStage extends Model
-{
-   use HasFactory, SoftDeletes; // 🔥 AJOUTER SoftDeletes
+class OffreStage extends Model{
+   use HasFactory, SoftDeletes; 
 
     protected $table = 'offres_stage';
     protected $primaryKey = 'idOffre';
