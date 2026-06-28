1edf6775e09ae5b7ee25720bbcf41b1c94cc511a
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 19:36:05 +0100

    feat(notification) : ajout de la prise en charge des icônes pour les notifications

--- stage-platform/database/migrations/2026_06_17_164359_add_icone_to_notifications_table.php
+++ stage-platform/database/migrations/2026_06_17_164359_add_icone_to_notifications_table.php
@@ -4,17 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up()
-    {
+return new class extends Migration{
+    public function up(){
         Schema::table('notifications', function (Blueprint $table) {
             $table->string('icone')->nullable()->after('type');
         });
     }
 
-    public function down()
-    {
+    public function down(){
         Schema::table('notifications', function (Blueprint $table) {
             $table->dropColumn('icone');
         });
