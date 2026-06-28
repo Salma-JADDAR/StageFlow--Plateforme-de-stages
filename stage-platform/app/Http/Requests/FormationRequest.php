590cd4f7aff7844222450c51f7c521bebcb7a498
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 22:00:59 +0100

    feat(validation) : création du FormRequest pour les formations

--- stage-platform/app/Http/Requests/FormationRequest.php
+++ stage-platform/app/Http/Requests/FormationRequest.php
@@ -4,11 +4,9 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class FormationRequest extends FormRequest
-{
+class FormationRequest extends FormRequest{
     public function authorize() { return true; }
-    public function rules()
-    {
+    public function rules(){
         return [
             'diplome' => 'required|string',
             'etablissement' => 'required|string',
