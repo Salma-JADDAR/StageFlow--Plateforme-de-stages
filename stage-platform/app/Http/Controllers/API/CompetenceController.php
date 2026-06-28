f85093afe9ebfee66f867d220061e45bd7a919bb
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 20:19:44 +0100

    feat(competences) : implémentation de la récupération de la liste des compétences

--- stage-platform/app/Http/Controllers/API/CompetenceController.php
+++ stage-platform/app/Http/Controllers/API/CompetenceController.php
@@ -8,8 +8,7 @@ use Illuminate\Http\Request;
 
 class CompetenceController extends Controller
 {
-    public function index()
-    {
+    public function index(){
         return response()->json(Competence::all());
     }
 
