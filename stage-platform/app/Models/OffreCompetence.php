c56d293bc4e3cd5292084cd1b785ba3119b3a090
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 19:55:20 +0100

    feat(model) : ajout du modèle OffreCompetence pour la gestion des relations et des priorités

--- stage-platform/app/Models/OffreCompetence.php
+++ stage-platform/app/Models/OffreCompetence.php
@@ -4,8 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class OffreCompetence extends Pivot
-{
+class OffreCompetence extends Pivot{
     protected $table = 'offre_competence';
     protected $fillable = ['priorite'];
 }
