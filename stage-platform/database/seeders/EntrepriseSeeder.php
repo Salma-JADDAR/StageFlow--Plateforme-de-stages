7b78837f21180aa3a3521bfe0747b14715ef1274
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 19:49:08 +0100

    feat(seeder) : implémentation du seeder des entreprises avec génération des données de démonstration

--- stage-platform/database/seeders/EntrepriseSeeder.php
+++ stage-platform/database/seeders/EntrepriseSeeder.php
@@ -5,10 +5,8 @@ namespace Database\Seeders;
 use App\Models\Entreprise;
 use Illuminate\Database\Seeder;
 
-class EntrepriseSeeder extends Seeder
-{
-    public function run()
-    {
+class EntrepriseSeeder extends Seeder{
+    public function run(){
         Entreprise::factory(10)->create();
     }
 }
