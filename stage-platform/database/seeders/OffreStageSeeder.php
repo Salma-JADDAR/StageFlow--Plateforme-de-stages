086eb1f18d0b0daec4b07d457d5e89b545e6312d
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 19:46:19 +0100

    feat(seeder) : implémentation du seeder des offres de stage avec génération des données de démonstration

--- stage-platform/database/seeders/OffreStageSeeder.php
+++ stage-platform/database/seeders/OffreStageSeeder.php
@@ -5,10 +5,8 @@ namespace Database\Seeders;
 use App\Models\OffreStage;
 use Illuminate\Database\Seeder;
 
-class OffreStageSeeder extends Seeder
-{
-    public function run()
-    {
+class OffreStageSeeder extends Seeder{
+    public function run(){
         OffreStage::factory(30)->create();
     }
 }
