8eed7dd33fd944d012a14ca1fab66a6913bfb5cf
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 19:53:59 +0100

    feat(model) : implémentation du modèle Recommendation avec gestion des scores de correspondance

--- stage-platform/app/Models/Recommendation.php
+++ stage-platform/app/Models/Recommendation.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Recommendation extends Model
-{
+class Recommendation extends Model{
     use HasFactory;
 
     protected $primaryKey = 'idRecommendation';
