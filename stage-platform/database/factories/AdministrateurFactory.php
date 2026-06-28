3edc63204036852b1e3dcba0e0a92e0d505a9589
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 19:43:52 +0100

    feat(factory) : implémentation de la factory des administrateurs avec génération des comptes associés

--- stage-platform/database/factories/AdministrateurFactory.php
+++ stage-platform/database/factories/AdministrateurFactory.php
@@ -5,12 +5,10 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class AdministrateurFactory extends Factory
-{
+class AdministrateurFactory extends Factory{
     protected $model = \App\Models\Administrateur::class;
 
-    public function definition()
-    {
+    public function definition(){
         return [
             'user_id' => User::factory()->create(['role' => 'admin'])->id,
         ];
