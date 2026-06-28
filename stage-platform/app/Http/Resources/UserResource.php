97074fa2f116c9e3b62df82a5dca98041cc97663
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 22:12:50 +0100

    feat(api) : ajout du resource User pour structurer les utilisateurs

--- stage-platform/app/Http/Resources/UserResource.php
+++ stage-platform/app/Http/Resources/UserResource.php
@@ -4,10 +4,8 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource
-{
-    public function toArray($request)
-    {
+class UserResource extends JsonResource{
+    public function toArray($request){
         return [
             'id' => $this->id,
             'nom' => $this->nom,
