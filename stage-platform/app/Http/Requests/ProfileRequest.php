96184b6627a4acc9f6c17f4cca3cccf6ad7d3544
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 22:03:08 +0100

    feat(profile) : ajout de la validation des données de profil utilisateur

--- stage-platform/app/Http/Requests/ProfileRequest.php
+++ stage-platform/app/Http/Requests/ProfileRequest.php
@@ -4,8 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ProfileRequest extends FormRequest
-{
+class ProfileRequest extends FormRequest{
     public function authorize() { return true; }
     public function rules()
     {
