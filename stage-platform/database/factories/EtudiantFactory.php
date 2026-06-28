a59cd090e8481cbcc57122b31c5f3c9f59911a57
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 19:40:21 +0100

    feat(factory) : implémentation de la factory des étudiants avec génération des profils utilisateurs

--- stage-platform/database/factories/EtudiantFactory.php
+++ stage-platform/database/factories/EtudiantFactory.php
@@ -5,12 +5,10 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class EtudiantFactory extends Factory
-{
+class EtudiantFactory extends Factory{
     protected $model = \App\Models\Etudiant::class;
 
-    public function definition()
-    {
+    public function definition(){
         return [
             'user_id' => User::factory()->create(['role' => 'etudiant'])->id,
             'photo' => $this->faker->imageUrl(200, 200, 'people'),
