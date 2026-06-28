3b765cac5b8648c37e3af7a546a8e158b393adfc
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 21:59:48 +0100

    feat(validation) : création du FormRequest pour l’upload CV

--- stage-platform/app/Http/Requests/CVRequest.php
+++ stage-platform/app/Http/Requests/CVRequest.php
@@ -4,15 +4,12 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CVRequest extends FormRequest
-{
-    public function authorize()
-    {
+class CVRequest extends FormRequest{
+    public function authorize(){
         return true;
     }
 
-    public function rules()
-    {
+    public function rules(){
         return [
             'cv' => 'required|file|mimes:pdf,doc,docx|max:5120'
         ];
