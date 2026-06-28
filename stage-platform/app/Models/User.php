83273a6e8a50dd5071215045fa0f263f0004b397
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 19:52:28 +0100

    feat(model) : implémentation du modèle User avec prise en charge de Sanctum et des notifications

--- stage-platform/app/Models/User.php
+++ stage-platform/app/Models/User.php
@@ -6,14 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-class User extends Authenticatable
-{
+class User extends Authenticatable{
      use HasApiTokens, HasFactory, Notifiable;
 
-    // app/Models/User.php
-protected $fillable = [
+     protected $fillable = [
     'nom', 'prenom', 'email', 'password', 'role', 'dernierAcces'
-];
+    ];
 
     protected $hidden = ['password', 'remember_token'];
 
