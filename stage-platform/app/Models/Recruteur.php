f2d4116904b1b53b0ba9e0977e168ede3ef72050
Salma GTU <salmajaddar7@gmail.com>
2026-06-28 19:53:16 +0100

    feat(model) : implémentation du modèle Recruteur avec gestion des relations utilisateur et entreprise

--- stage-platform/app/Models/Recruteur.php
+++ stage-platform/app/Models/Recruteur.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Recruteur extends Model
-{
+class Recruteur extends Model{
     use HasFactory;
 
     protected $primaryKey = 'idRecruteur';
