eefe7325dceb1f468007502808c05d5a30df41dc
Salma GTU <salmajaddar7@gmail.com>
2026-06-29 10:00:54 +0100

    feat(ui) : implémentation de la structure principale de l'interface

--- frontend-react/src/layouts/MainLayout.jsx
+++ frontend-react/src/layouts/MainLayout.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-
 export default function MainLayout() {
   return (
     <div className="min-h-screen bg-gray-50">
